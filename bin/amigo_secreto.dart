4d415e85e14b24c00eb4b8a35f1ae49d7d892493
Felipe Vargas <felipelwvargas@gmail.com>
2025-11-06 12:57:06 -0300

    first commit

---
+++ bin/amigo_secreto.dart
@@ -0,0 +1,5 @@
+import 'package:amigo_secreto/amigo_secreto.dart' as amigo_secreto;
+
+void main(List<String> arguments) {
+  print('Hello world: ${amigo_secreto.calculate()}!');
+}
---
+++ test/amigo_secreto_test.dart
@@ -0,0 +1,8 @@
+import 'package:amigo_secreto/amigo_secreto.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
